4a0f4f044533ffd46f8bc6189a67af82b295bdb1
Achel <achelmichael80@gmail.com>
2025-03-29 12:41:22 +0000

    Prepare for Render deployment

--- accounting_system/accounting_system/settings.py
+++ accounting_system/accounting_system/settings.py
@@ -135,3 +135,5 @@ LOGIN_URL = '/login/'
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware") 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
